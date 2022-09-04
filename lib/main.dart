00d788b5ab1f453c18940830fe1e47742818ee77
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-09-04 10:36:12 +0900

    Modify:sample code

--- lib/main.dart
+++ lib/main.dart
@@ -2,55 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  // .envファイルの読み込み
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              "↓ルートディレクトリ配下の.envに定義した環境変数を表示\n",
-            ),
-            Text(
-              "【TEST_KEY】：${dotenv.env['TEST_KEY']}",
-            ),
-            Text(
-              "【TEST_ID】：${dotenv.env['TEST_ID']}",
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("flutter_dotenv sample app"),
+        ),
+        body: Center(
+          // .envに設定した環境変数の呼び出し
+          child: Text("【TEST_KEY】：${dotenv.env['TEST_KEY']}"),
         ),
       ),
     );
