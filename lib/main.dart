4336a1ede2ee1ab05c7446566996559ac528b99f
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-09-04 11:22:57 +0900

    Modify sample code: remove . version

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   // .envファイルの読み込み
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "env");
   runApp(const MyApp());
 }
 
