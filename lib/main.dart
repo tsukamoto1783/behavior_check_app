23c3127d090aeaadef2c70d31a3c8ea3adaf6f2c
matttake <t.fr.swim@icloud.com>
2022-10-14 18:59:30 +0900

    dotenv behavior chack

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,26 @@
 import 'package:flutter/material.dart';
-import 'js/js_func_call.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  // .envファイルの読み込み
+  await dotenv.load(fileName: "env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    const argumentValue = "js関数の呼び出し";
-    String returnValue = "";
-
     return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("js operation check"),
-          ),
-          body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                // JavaScript関数を呼び出し、戻り値をコンソールに表示
-                returnValue = callJsFunc(argumentValue);
-                debugPrint(returnValue);
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.play_circle,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ));
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("flutter_dotenv sample app"),
+        ),
+        body: Center(
+          // .envに設定した環境変数の呼び出し
+          child: Text("【TEST_KEY】：${dotenv.env['TEST_KEY']}"),
+        ),
+      ),
+    );
   }
 }
